0d7e29cd21c64c284cd0091808029b436a9fef9a
Harsh Yadav <raoharsh138@gmail.com>
2023-08-22 04:35:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtn = document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
+// const rock = '&#x1F5FB;';
+// const paper = '&#128196;';
+// const scissors = '&#x2702;';
+
+
+
+choiceBtn.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
+
+
+function computerTurn() {
+
+    const computerChoices = ["🪨", "📄", "✂️", "🔥"]
+    let randomNum = Math.floor(Math.random() * computerChoices.length);
+
+    computer = computerChoices[randomNum];
+
+
+    // switch (randomNum) {
+    //     case 1:
+    //         computer = '🪨';
+    //         break;
+    //     case 2:
+    //         computer = '📄';
+    //         break;
+
+    //     case 3:
+    //         computer = '✂️';
+    //         break;
+    //     case 4:
+    //         computer = '🔥';
+    //         break;
+    // }
+    // console.log(computer);
+
+}
+
+function checkWinner() {
+    if (player == computer) {
+        return 'DRAW!!';
+    }
+    else if (player == '🔥') {
+        return "You WIN!!!!";
+    }
+    else if (computer == '🪨') {
+        return (player == '📄') ? "You Win!" : "You Lose!";
+    }
+    else if (computer == '📄') {
+        return (player == '✂️') ? "You Win!" : "You Lose!";
+    }
+    else if (computer == '✂️') {
+        return (player == '🪨') ? "You Win!" : "You Lose!";
+    }
+    else if (computer == '🔥') {
+        return "You Lose!!";
+    }
+
+}
+
+
+
